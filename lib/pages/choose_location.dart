70e258995d29f28fb57dbab1d8b01c472dd42917
shashank <shash.201cs257@gmail.com>
2021-06-03 21:53:45 +0530

    Added Bottom Navigation Bar to both pages

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,12 +6,39 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  var  L = ['/home','/choose_location'];
+  int _selectedIndex =0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      Navigator.pushNamed(context, L[_selectedIndex]);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+          backgroundColor: Colors.red,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.edit_location),
+          label: 'Edit location',
+          backgroundColor: Colors.green,
+        ),
+
+      ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+
+      ),
       appBar: AppBar(
         title: Text("Choose Location"),
       ),
+
 
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,9 +6,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var  L = ['/home','/choose_location'];
+  int _selectedIndex =0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      Navigator.pushNamed(context, L[_selectedIndex]);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+          backgroundColor: Colors.red,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.edit_location),
+          label: 'Edit location',
+          backgroundColor: Colors.green,
+        ),
+
+      ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+
+      ),
       body: SafeArea(
         child: Column(
           children: <Widget>[
